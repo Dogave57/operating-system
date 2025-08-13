1c7493a88fce10cf80d5f04f821ac90db49ec2ce
Dogave57 <diddyenjoyer4@gmail.com>
2025-08-12 23:58:00 -0500

    added some stuff for the filesystem

--- filesystem.h
+++ filesystem.h
@@ -2,17 +2,14 @@
 #define _FILESYSTEM
 #include <stdint.h>
 #include "kernel.h"
-struct fs_link{
-	char filename[32];
-	unsigned int linksize;
-	unsigned int index;
-	unsigned int parent;
-	unsigned int left;
-	unsigned int right;
-	char data[1];
-};
-int read_sectors(unsigned int drive, uint32_t sector, uint8_t sectorcnt, uint16_t* buffer);
-int write_sectors(unsigned int drive, uint32_t sector, uint8_t sectorcnt, uint16_t* buffer);
+#define FS_SIGNATURE 0x56525458//VRTX in little endian
+#define FS_RESERVED_SECTORS 128
+struct filesystem_info{
+	unsigned int signature;
+	unsigned char fsname[16];
+}__attribute__((packed));
+int read_sectors(unsigned int drive, uint32_t sector, uint8_t sectorcnt, uint16_t* buffer, unsigned int bytespersector);
+int write_sectors(unsigned int drive, uint32_t sector, uint8_t sectorcnt, uint16_t* buffer, unsigned int bytespersector);
 int drive_getinfo(unsigned int drive, uint16_t* info);
 struct highlow_64 drive_getsectors(unsigned int drive);
 unsigned int getbootdrive(void);
